10d914e5fb88c5c1f54e39a083f96720cbf2d5de
alireza-shokri <lshkry88@gmail.com>
2024-04-11 23:39:51 +0330

    chang ditael

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const worold=[
     "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
     "q","r","s","t","u","v","w","x","y","z"," ",".",",","!",":","?",";"
     ]
-    let matn='Lorem ipsum  perspiciatis, sapiente, error possimus illum facilis accusantium?orelam minima. Lorem ipsum dos aperiam modi temporibus. Aut, solutlam ex.e!'
+    let matn='Lorem ipsum perspiciatis, sapiente, error possimus illum facilis accusantium?orelam minima. Lorem ipsum dos aperiam modi temporibus. Aut, solutlam ex.e!'
     let parentAllWrold=document.querySelector('.parent_all_world');
     let time=document.querySelector('.time');
     let all=document.getElementsByClassName('span_world');
     let refrsh=document.querySelector('.refrsh');
-    
+
     refrsh.addEventListener('click',refrshhandler);
-    document.body.addEventListener('keydown',my);
+    document.body.addEventListener('keydown',typingHandler);
 
     let shomar=0;
     let minut=0;
@@ -22,7 +22,8 @@ const worold=[
     let StrMinut;
     let StrSecend;
     let StrSadomSecend;
-    // crredet li/////////////////////////
+
+    // crredet li-----------------------------------
     for (var i=0;i<matn.length;i++){
         var newspan=document.createElement('span');
         newspan.innerHTML=matn[i];
@@ -31,12 +32,12 @@ const worold=[
         if(newspan.textContent==' ')  
             newspan.style.padding='8px';
 }
-    // tiem////////////////////////
+    // tiem-----------------------------------
     function fortime(){
        interval=setInterval(() => {
             sadomSecend++
 
-            if(sadomSecend===60){ secend++; sadomSecend=0;   
+            if(sadomSecend===100){ secend++; sadomSecend=0;   
                 if(secend===60){minut++;secend=0;}}
 
             StrSadomSecend=String(sadomSecend).padStart(2,'0');
@@ -46,7 +47,7 @@ const worold=[
             document.querySelector('.secend_span').textContent=StrSecend;
             document.querySelector('.minut_span').textContent=StrMinut;
 
-            // clear_interval///////////
+            // clear_interval------------------------
            if(all.length==shomar){
                clearInterval(interval);
            }
@@ -54,7 +55,7 @@ const worold=[
     }
 
 
-    // speed/////////////////////
+    // speed-----------------------------------
     function speedd(){
         let intervalspeed=setInterval(() => {
             // timestat-time end
@@ -67,17 +68,24 @@ const worold=[
     }
 
 
-    // bordar/////////
+    // bordar-----------------------------------
     function bordarword(){
-        if(shomar+1<all.length){  all[shomar+1].style.border='solid 1px'; all[shomar].style.border='none';}
+        if(shomar+1<all.length){  all[shomar+1].style.border='solid 1px aqua'; all[shomar].style.border='none';}
     }
 
+    //  scroo-----------------------------------
+    function checkScroll() {
+        if (shomar === all.length / 2) {
+            const currentWordPosition = all[shomar].getBoundingClientRect();
+            window.scrollTo(0, currentWordPosition.top - window.innerHeight / 2);
+        }
+    }
+    // onkeydown-----------------------------------
+    all[shomar].style.border='solid 1px aqua';
 
-    // onkeydown///////////////////////////////
-    all[shomar].style.border='solid 1px';
-    function my(event){
-         event.preventDefault();
-
+    function typingHandler(event){
+        event.preventDefault();
+        checkScroll()
        if (shomar<all.length||event.key=='Backspace'){
             if (worold.includes(event.key.toLowerCase())){
 
@@ -89,7 +97,7 @@ const worold=[
                     vazet=true;
                 }
                 if(event.key===matn[shomar]){
-                    all[shomar].style.background='blue';
+                    all[shomar].style.color='green';
                     bordarword();
                     shomar++;
                 }
@@ -104,10 +112,12 @@ const worold=[
                     fortime();
                     speedd();
                 }
-                all[shomar-1].style.background='none';
+                if(all[shomar-1].style.color){ all[shomar-1].style.color='wheat';}
+                if(all[shomar-1].style.background){ all[shomar-1].style.background='none';}
+               
                 if(shomar==all.length){all[shomar-1].style.border='none'; }
                 else {all[shomar].style.border='none';} 
-                all[shomar-1].style.border='solid 1px';
+                all[shomar-1].style.border='solid 1px aqua';
                 shomar--;
             }
             if(all.length/2==shomar){
@@ -119,14 +129,14 @@ const worold=[
     }
 
 
-    // refrsh//////////////////////////////
+    // refrsh-----------------------------------
     function refrshhandler(){
         if(shomar==all.length){all[shomar-1].style.border='none'; }
         else {all[shomar].style.border='none';} 
 
         clearInterval(interval);
 
-        for(var t=0;shomar>t;shomar--){all[shomar-1].style.background='none';}
+        for(var t=0;shomar>t;shomar--){all[shomar-1].style.background='none';all[shomar-1].style.color='wheat'}
         document.querySelector('.sadomsecend_span').textContent='00';
         document.querySelector('.secend_span').textContent='00';
         document.querySelector('.minut_span').textContent='00';
@@ -137,7 +147,7 @@ const worold=[
         shomar=0;
         timeStart=0;
         allTime=0;
-        document.querySelector('.speed_time').textContent='';
+        document.querySelector('.speed_time').textContent='0';
         vazet=false;
-        all[shomar].style.border='solid 1px black';
+        all[shomar].style.border='solid 1px aqua';
     }
